fc3137a9f618b3f7835951ae0cd2819a88be230c
Tuyen <hnt4499@gmail.com>
2021-05-21 12:34:19 +0800

    Add requirements to setup scripts

--- setup.py
+++ setup.py
@@ -6,9 +6,33 @@
 
 
 from setuptools import setup
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+from subprocess import check_call
 
 with open("README.md") as f:
     readme = f.read()
+
+
+class PostDevelopCommand(develop):
+    """
+    Post-installation for development mode.
+    Source: https://stackoverflow.com/a/36902139
+    """
+    def run(self):
+        develop.run(self)
+        check_call("python3 -m spacy download en_core_web_sm".split())
+
+
+class PostInstallCommand(install):
+    """
+    Post-installation for installation mode.
+    Source: https://stackoverflow.com/a/36902139
+    """
+    def run(self):
+        install.run(self)
+        check_call("python3 -m spacy download en_core_web_sm".split())
+
 
 setup(
     name="dpr",
@@ -38,5 +62,11 @@ setup(
         "spacy>=2.1.8",
         "hydra-core>=1.0.0",
         "omegaconf>=2.0.1",
+        "nltk==3.6.2",
+        "jsonlines",
     ],
+    cmdclass={  # post install commands
+        'develop': PostDevelopCommand,
+        'install': PostInstallCommand,
+    },
 )
